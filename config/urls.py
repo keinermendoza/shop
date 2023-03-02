1126f47d9843cc01853f6e80fb082932851001e3
keinermendoza <keinermendoza@gmail.com>
2023-03-02 10:12:57 -0400

    views for cart

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart'),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 def product_list(request, category_slug=None):
     category = None
@@ -20,6 +21,9 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product_detail.html',
-                  {'product': product})
+                  {'product': product,
+                  'cart_product_form':cart_product_form})
