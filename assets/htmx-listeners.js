55f98b358879834186aa84044f7920df473e6242
keiner <keinermendoza@gmail.com>
2023-12-09 15:12:07 -0400

    fixing bug and cleaning the code using Alpine.store

--- assets/htmx-listeners.js
+++ assets/htmx-listeners.js
@@ -7,39 +7,47 @@ document.addEventListener("htmx:confirm", function(e) {
         title: "Proceed?",
         text: `I ask you... ${e.detail.question}`
       }).then(function(result) {
-        // Si el usuario confirma, continuar con la solicitud htmx
         if (result.isConfirmed) {
-            // const deleteItemData = new CustomEvent('deleteItemData', {detail : 'hola desde el custom event'})
-            // window.dispatchEvent(deleteItemData)
+   
             console.log('adentro del wsal')
             e.detail.issueRequest(true); 
 
-
-            // cuando uso simple event con add eventlistener vanilla funciona
-            // la version vanilla del custom event tambien funciona
-
-            const event = new CustomEvent("deleterow", {detail : {id : e.target.dataset.rowid}});
+            const event = new CustomEvent("deleterow", {detail : {itemId : e.target.dataset.rowid}});
             window.dispatchEvent(event);
 
-            // Dispatch the event.
         }
       });
     }
   });
 
-  // const event = new CustomEvent("build", { detail: elem.dataset.time });
 
-// document.addEventListener('DOMContentLoaded', () => {
- 
+document.addEventListener('alpine:init', () => {
+  Alpine.store('cart', {
+    items: [],
+    totalPrice: 0,
+    countItems: 0,
 
-// // Listen for the event.
-// window.addEventListener(
-//   "hello",
-//   (e) => {
-//     console.log('hola', e.detail.id)
-//   },
-//   false,
-// );
+    getTotalPrice(n) {
+      this.totalPrice = parseFloat(n).toFixed(2);
+    },
+    getCountItems(n) {
+      this.countItems = parseInt(n);
+    },
+    updateTotals() {
+        this.countItems = this.items.reduce((sum, item) => sum + item.quantity, 0);
+        this.totalPrice = (this.items.reduce((sum, item) => sum + item.quantity * item.price, 0)).toFixed(2)
+    },
 
+    updateItemQuantity(newQuantity, itemId) {
+      this.items[this.items.findIndex(item => item.id === itemId)].quantity = newQuantity;
+      this.updateTotals();
+    },
 
-// })
+    deleteItem(itemId) {
+      this.items = this.items.filter(item => item.id != itemId);
+      this.updateTotals();
+    }
+
+  })
+
+})
