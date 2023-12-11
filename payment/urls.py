35f4517f37f5fce65fc761e0a836b7c60d127720
keiner <keinermendoza@gmail.com>
2023-12-10 21:31:09 -0400

    adding webhook for mark the orders as paid

--- payment/urls.py
+++ payment/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('process/', views.payment_process, name='process'),
     path('completed/', views.payment_completed, name='completed'),
     path('canceled/', views.payment_canceled, name='canceled'),
+    path('webhook/', views.stripe_webhook, name='stripe-webhook'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.conf import settings
@@ -9,51 +11,85 @@ from orders.models import Order
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
+
 def payment_process(request):
-    order_id = request.session.get('order_id', None)
+    order_id = request.session.get("order_id", None)
     order = get_object_or_404(Order, id=order_id)
 
-    if request.method == 'POST':
-        success_url = request.build_absolute_uri(reverse('payment:completed'))
-        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
-        
+    if request.method == "POST":
+        success_url = request.build_absolute_uri(reverse("payment:completed"))
+        cancel_url = request.build_absolute_uri(reverse("payment:canceled"))
+
         # Stripe checkout session data
         session_data = {
-            'mode': 'payment',
-            'client_reference_id': order.id,
-            'success_url': success_url,
-            'cancel_url': cancel_url,
-            'line_items': []
+            "mode": "payment",
+            "client_reference_id": order.id,
+            "success_url": success_url,
+            "cancel_url": cancel_url,
+            "line_items": [],
         }
-    
+
         # add order items to the Stripe checkout session
         for item in order.items.all():
-
             # each element has all the relevant product information
-            session_data['line_items'].append({
-                'price_data': {
-                    'unit_amount': int(item.price * Decimal('100')),
-                    'currency': 'usd',
-                    'product_data': {
-                    'name': item.product.name,
+            session_data["line_items"].append(
+                {
+                    "price_data": {
+                        "unit_amount": int(item.price * Decimal("100")),
+                        "currency": "usd",
+                        "product_data": {
+                            "name": item.product.name,
+                        },
                     },
-                },
-                'quantity': item.quantity,
-            })
-
-            
+                    "quantity": item.quantity,
+                }
+            )
 
         session = stripe.checkout.Session.create(**session_data)
-    
+
         # redirect to Stripe payment form
         return redirect(session.url, code=303)
-    
+
     else:
-        return render(request, 'payment/process.html', {'order':order})
+        return render(request, "payment/process.html", {"order": order})
+
 
 def payment_completed(request):
-    return render(request, 'payment/completed.html')
-    
-def payment_canceled(request):
-    return render(request, 'payment/canceled.html')
+    return render(request, "payment/completed.html")
 
+
+def payment_canceled(request):
+    return render(request, "payment/canceled.html")
+
+
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+    event = None
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    
+    if event.type == 'checkout.session.completed':
+        session = event.data.object
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+                # mark order as paid
+            else:
+                order.paid = True
+                order.save()
+    return HttpResponse(status=200)
